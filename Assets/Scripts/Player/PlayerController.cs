c2f4a30831c10e7fa60921e5bfc988e01b4a1434
Martin Viidik <Martinviidik@gmail.com>
2020-07-25 00:33:31 +0200

    Added player movement functionality

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float baseSpeed = 1.0f;
+
+    [SerializeField]
+    private Vector2 movementDirection;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Animator animator;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+        Animate();
+    }
+
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * baseSpeed;
+    }
+
+    void Animate()
+    {
+        if(movementDirection != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movementDirection.x);
+            animator.SetFloat("Vertical", movementDirection.y);
+        }
+        animator.SetFloat("Speed", movementSpeed);
+    }
+}

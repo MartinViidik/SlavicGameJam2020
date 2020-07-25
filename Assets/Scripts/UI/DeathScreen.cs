d06e8539a4f4e689cfaa6b6c3dc7373c46f8279f
Martin Viidik <Martinviidik@gmail.com>
2020-07-25 17:50:53 +0200

    Added death screen

---
+++ Assets/Scripts/UI/DeathScreen.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DeathScreen : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text jap;
+
+    [SerializeField]
+    private TMP_Text eng;
+
+    [SerializeField]
+    private string[] jap_proverb;
+
+    [SerializeField]
+    private string[] eng_proverb;
+
+    [SerializeField]
+    private GameObject ingameUI;
+
+    private static DeathScreen _instance;
+
+    public static DeathScreen Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        int RNG = Random.Range(0, jap_proverb.Length);
+        jap.text = jap_proverb[RNG];
+        eng.text = eng_proverb[RNG];
+        ingameUI.SetActive(false);
+    }
+
+}

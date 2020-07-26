0da20d6ef32d27b036f5faa972511e75bf50a221
Martin Viidik <Martinviidik@gmail.com>
2020-07-26 13:18:08 +0200

    Added main menu

---
+++ Assets/Scripts/UI/LevelChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    private static LevelChanger _instance;
+    public static LevelChanger Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Menu;
+    [SerializeField]
+    private GameObject About;
+    private void Awake()
+    {
+        ShowMenu();
+    }
+    public void StartGame()
+    {
+        LevelChanger.Instance.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowMenu()
+    {
+        Menu.SetActive(true);
+        About.SetActive(false);
+    }
+
+    public void ShowAbout()
+    {
+        Menu.SetActive(false);
+        About.SetActive(true);
+    }
+
+}

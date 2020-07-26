eaa074f87326e5fa2e35c6f2e56816b799b8c8d7
Sylwester Wróblewski <sylwek.wroblewski@gmail.com>
2020-07-26 13:17:10 +0200

    Added monster spawner prefab

---
+++ Assets/Scripts/Items/EnemySopawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySopawner : MonoBehaviour {
+
+    [SerializeField] public List<GameObject> monstersToSpawn;
+
+    public float waitTime = 1f;
+    private bool cooldown;
+
+    
+    void FixedUpdate() {
+        if (cooldown) return;
+        var RNG = Random.Range(0, monstersToSpawn.Count);
+        Instantiate(monstersToSpawn[RNG], transform);
+        StartCoroutine("Cooldown");
+
+    }
+
+    private IEnumerator Cooldown() {
+        cooldown = true;
+        yield return new WaitForSeconds(waitTime);
+        cooldown = false;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawIcon(transform.position, "BuildSettings.Lumin.small");
+    }
+
+}
